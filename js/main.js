6883587c7d64c3bdbf4b4d5bbc58ee733b24cf41
ExodusDG <sanistray@gmail.com>
2022-08-05 19:19:18 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,59 @@
-particlesJS.load('particles-js', 'js/particlesjs-config.json', function () {
-    console.log('callback - particles.js config loaded');
-});
+const element = document.querySelector(".worldContent");
+var scrollCounter = 0;
 
-particlesJS.load('particles-js-2', 'js/particlesjs-config.json', function () {
-    console.log('callback - particles.js config loaded');
-});
 
-particlesJS.load('particles-js-3', 'js/particlesjs-config.json', function () {
-    console.log('callback - particles.js config loaded');
+function blockScroll() {
+    $('body').addClass('scrollDisabled')
+}
+function allowScroll() {
+    $('body').removeClass('scrollDisabled')
+}
+
+element.addEventListener('wheel', (e) => {
+    e.preventDefault();
+
+    if (scrollCounter > 9) {
+
+        if ($('.worldContent').hasClass('horizontalScrollOff')) {
+
+        } else {
+            $('.worldContent').addClass('horizontalScrollOff')
+
+            setTimeout(() => {
+                $('.worldContent').removeClass('horizontalScrollOff')
+            }, 500);
+        }
+
+        scrollCounter = 9;
+
+    } else if (scrollCounter < 0) {
+        scrollCounter = 0;
+
+        if ($('.worldContent').hasClass('horizontalScrollOff')) {
+
+        } else {
+            $('.worldContent').addClass('horizontalScrollOff')
+
+            setTimeout(() => {
+                $('.worldContent').removeClass('horizontalScrollOff')
+            }, 500);
+        }
+    } else {
+        wDelta = e.wheelDelta < 0 ? 'down' : 'up';
+        if (wDelta == 'down') {
+            scrollCounter++;
+            var counterTranslate = scrollCounter * 10;
+            $('.worldsSliderWrapper').attr('style', 'transform: translate3d(-' + counterTranslate + '%, 0px, 0px);')
+        } else {
+            scrollLeft()
+        }
+    }
+
+    function scrollLeft() {
+        scrollCounter--;
+        var counterTranslate = scrollCounter * 10;
+        $('.worldsSliderWrapper').attr('style', 'transform: translate3d(-' + counterTranslate + '%, 0px, 0px);')
+    }
 });
 
 var screenWidth = $(window).width()
@@ -105,18 +151,6 @@ $(window).on('resize scroll', function () {
         }, 500);
     }
 
-    if ($('.sliderText_2').isInViewport2()) {
-        if (rightSide == true) {
-            return false;
-        } else {
-            if (mobile == false) {
-                blockScroll()
-                vertScroll()
-                vertScrollFirefox()
-            }
-        }
-    }
-
     if ($('.special').isInViewport()) {
         blackTheme()
     } else {
@@ -138,62 +172,6 @@ $(window).on('resize scroll', function () {
 
 });
 
-function blockScroll() {
-    $('body').addClass('scrollDisabled')
-}
-function allowScroll() {
-    $('body').removeClass('scrollDisabled')
-}
-
-function vertScrollFirefox() {
-    window.addEventListener("wheel", function (e) {
-        wDelta = e.wheelDelta < 0 ? 'down' : 'up';
-        if (wDelta == 'down') {
-            if (rightSide == true) {
-                allowScroll()
-            } else {
-                scrollRight()
-            }
-        }
-    }, { passive: false });
-}
-
-
-function vertScroll() {
-    window.addEventListener("mousewheel", function (e) {
-        wDelta = e.wheelDelta < 0 ? 'down' : 'up';
-        if (wDelta == 'down') {
-            if (rightSide == true) {
-                allowScroll()
-            } else {
-                scrollRight()
-            }
-        }
-    }, { passive: false });
-}
-
-var maxWidth = 0;
-
-$.each($('.sliderItem'), function (key, value) {
-    maxWidth = maxWidth + $(this).width() + 100
-})
-
-
-
-
-
-function scrollRight() {
-    var currentTranslate = Number($('.worldsSliderWrapper').attr('style').replace('transform: translateX(-', '').replace('px);', ''))
-    var newTranslate = currentTranslate + 30;
-
-    if (newTranslate > (maxWidth - screenWidth)) {
-        rightSide = true;
-        leftSide = false;
-        return false;
-    } else {
-        $('.worldsSliderWrapper').attr('style', 'transform: translateX(-' + newTranslate + 'px);')
-    }
-}
 
 
 function blackTheme() {
@@ -213,209 +191,284 @@ function whiteTheme() {
     $('.special').removeClass('specialBlack')
 }
 
-new Skroll()
-    .add(".headerText h1", {
-        delay: 500,
-        duration: 1000,
-        animation: "slideInLeft"
-    })
-    .init();
 
-new Skroll()
-    .add(".headerText p", {
-        delay: 700,
-        duration: 1000,
-        animation: "slideInLeft"
-    })
-    .init();
+websiteAnimations()
 
-new Skroll()
-    .add(".siteTitle p", {
-        delay: 300,
-        duration: 500,
-        animation: "fadeInDown"
-    })
-    .init();
+function websiteAnimations() {
+    new Skroll()
+        .add(".headerText h1", {
+            delay: 500,
+            duration: 1000,
+            animation: "slideInLeft"
+        })
+        .init();
 
-new Skroll()
-    .add(".siteTitle h1", {
-        delay: 100,
-        duration: 500,
-        animation: "fadeInDown"
-    })
-    .init();
+    new Skroll()
+        .add(".headerText p", {
+            delay: 700,
+            duration: 1000,
+            animation: "slideInLeft"
+        })
+        .init();
 
-new Skroll()
-    .add(".nmvcText-1", {
-        delay: 100,
-        duration: 500,
-        animation: "fadeInDown"
-    })
-    .init();
+    new Skroll()
+        .add(".siteTitle p", {
+            delay: 300,
+            duration: 500,
+            animation: "fadeInDown"
+        })
+        .init();
 
-new Skroll()
-    .add(".nmvcText-2", {
-        delay: 200,
-        duration: 500,
-        animation: "fadeInDown"
-    })
-    .init();
+    new Skroll()
+        .add(".siteTitle h1", {
+            delay: 100,
+            duration: 500,
+            animation: "fadeInDown"
+        })
+        .init();
 
-new Skroll()
-    .add(".nmvcText-3", {
-        delay: 300,
-        duration: 500,
-        animation: "fadeInDown"
-    })
-    .init();
+    new Skroll()
+        .add(".nmvcText-1", {
+            delay: 100,
+            duration: 500,
+            animation: "fadeInDown"
+        })
+        .init();
 
-new Skroll()
-    .add(".plotTable", {
-        delay: 300,
-        duration: 700,
-        animation: "slideInLeft"
-    })
-    .init();
+    new Skroll()
+        .add(".nmvcText-2", {
+            delay: 200,
+            duration: 500,
+            animation: "fadeInDown"
+        })
+        .init();
 
+    new Skroll()
+        .add(".nmvcText-3", {
+            delay: 300,
+            duration: 500,
+            animation: "fadeInDown"
+        })
+        .init();
 
-new Skroll()
-    .add(".plotImage", {
-        delay: 300,
-        duration: 700,
-        animation: "slideInRight"
-    })
-    .init();
+    new Skroll()
+        .add(".plotTable", {
+            delay: 300,
+            duration: 700,
+            animation: "slideInLeft"
+        })
+        .init();
 
 
-
-new Skroll()
-    .add(".specialCard-01", {
-        delay: 100,
-        duration: 500,
-        animation: "fadeInDown"
-    })
-    .init();
-
-
-new Skroll()
-    .add(".specialCard-02", {
-        delay: 300,
-        duration: 500,
-        animation: "fadeInDown"
-    })
-    .init();
-
-new Skroll()
-    .add(".specialCard-03", {
-        delay: 500,
-        duration: 500,
-        animation: "fadeInDown"
-    })
-    .init();
-
-new Skroll()
-    .add(".specialCard-04", {
-        delay: 700,
-        duration: 500,
-        animation: "fadeInDown"
-    })
-    .init();
-
-new Skroll()
-    .add(".quoteTitle-1", {
-        delay: 100,
-        duration: 900,
-        animation: "slideInLeft"
-    })
-    .init();
+    new Skroll()
+        .add(".plotImage", {
+            delay: 300,
+            duration: 700,
+            animation: "slideInRight"
+        })
+        .init();
 
 
-new Skroll()
-    .add(".quoteTitle-2", {
-        delay: 300,
-        duration: 900,
-        animation: "slideInRight"
-    })
-    .init();
 
-new Skroll()
-    .add(".quoteTextBottom", {
-        delay: 600,
-        duration: 400,
-        animation: "fadeInUp"
-    })
-    .init();
+    new Skroll()
+        .add(".specialCard-01", {
+            delay: 100,
+            duration: 500,
+            animation: "fadeInDown"
+        })
+        .init();
 
 
-new Skroll()
-    .add(".goldHeader img", {
-        delay: 100,
-        duration: 900,
-        animation: "slideInLeft"
-    })
-    .init();
+    new Skroll()
+        .add(".specialCard-02", {
+            delay: 300,
+            duration: 500,
+            animation: "fadeInDown"
+        })
+        .init();
 
-new Skroll()
-    .add(".goldHeaderText", {
-        delay: 100,
-        duration: 900,
-        animation: "slideInRight"
-    })
-    .init();
+    new Skroll()
+        .add(".specialCard-03", {
+            delay: 500,
+            duration: 500,
+            animation: "fadeInDown"
+        })
+        .init();
+
+    new Skroll()
+        .add(".specialCard-04", {
+            delay: 700,
+            duration: 500,
+            animation: "fadeInDown"
+        })
+        .init();
+
+    new Skroll()
+        .add(".quoteTitle-1", {
+            delay: 100,
+            duration: 900,
+            animation: "slideInLeft"
+        })
+        .init();
 
 
-new Skroll()
-    .add(".gold-item-1", {
-        delay: 200,
-        duration: 400,
-        animation: "fadeInUp"
-    })
-    .init();
+    new Skroll()
+        .add(".quoteTitle-2", {
+            delay: 300,
+            duration: 900,
+            animation: "slideInRight"
+        })
+        .init();
 
-new Skroll()
-    .add(".gold-item-2", {
-        delay: 400,
-        duration: 400,
-        animation: "fadeInUp"
-    })
-    .init();
+    new Skroll()
+        .add(".quoteTextBottom", {
+            delay: 600,
+            duration: 400,
+            animation: "fadeInUp"
+        })
+        .init();
 
-new Skroll()
-    .add(".gold-item-3", {
-        delay: 600,
-        duration: 400,
-        animation: "fadeInUp"
-    })
-    .init();
 
-new Skroll()
-    .add(".gold-item-4", {
-        delay: 800,
-        duration: 400,
-        animation: "fadeInUp"
-    })
-    .init();
+    new Skroll()
+        .add(".goldHeader img", {
+            delay: 100,
+            duration: 900,
+            animation: "slideInLeft"
+        })
+        .init();
 
-new Skroll()
-    .add(".storesContent img", {
-        delay: 400,
-        duration: 400,
-        animation: "slideInLeft"
-    })
-    .init();
+    new Skroll()
+        .add(".goldHeaderText", {
+            delay: 100,
+            duration: 900,
+            animation: "slideInRight"
+        })
+        .init();
 
-new Skroll()
-    .add(".storesText", {
-        delay: 400,
-        duration: 400,
-        animation: "slideInRight"
-    })
-    .init();
 
-new Skroll()
-    .add(".stepsContent h1", {
-        delay: 400,
-        duration: 400,
-        animation: "fadeInDown"
-    })
-    .init();
+    new Skroll()
+        .add(".gold-item-1", {
+            delay: 200,
+            duration: 400,
+            animation: "fadeInUp"
+        })
+        .init();
+
+    new Skroll()
+        .add(".gold-item-2", {
+            delay: 400,
+            duration: 400,
+            animation: "fadeInUp"
+        })
+        .init();
+
+    new Skroll()
+        .add(".gold-item-3", {
+            delay: 600,
+            duration: 400,
+            animation: "fadeInUp"
+        })
+        .init();
+
+    new Skroll()
+        .add(".gold-item-4", {
+            delay: 800,
+            duration: 400,
+            animation: "fadeInUp"
+        })
+        .init();
+
+    new Skroll()
+        .add(".storesContent img", {
+            delay: 400,
+            duration: 400,
+            animation: "slideInLeft"
+        })
+        .init();
+
+    new Skroll()
+        .add(".storesText", {
+            delay: 400,
+            duration: 400,
+            animation: "slideInRight"
+        })
+        .init();
+
+    new Skroll()
+        .add(".stepsContent h1", {
+            delay: 400,
+            duration: 400,
+            animation: "fadeInDown"
+        })
+        .init();
+}
+
+function init() {
+    new SmoothScroll(document, 120, 12)
+}
+
+function SmoothScroll(target, speed, smooth) {
+    if (target === document)
+        target = (document.scrollingElement
+            || document.documentElement
+            || document.body.parentNode
+            || document.body) // cross browser support for document scrolling
+
+    var moving = false
+    var pos = target.scrollTop
+    var frame = target === document.body
+        && document.documentElement
+        ? document.documentElement
+        : target // safari is the new IE
+
+    target.addEventListener('mousewheel', scrolled, { passive: false })
+    target.addEventListener('wheel', scrolled, { passive: false })
+    target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
+
+    function scrolled(e) {
+        e.preventDefault(); // disable default scrolling
+
+        var delta = normalizeWheelDelta(e)
+
+        pos += -delta * speed
+        pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
+
+        if (!moving) update()
+    }
+
+    function normalizeWheelDelta(e) {
+        if (e.detail) {
+            if (e.wheelDelta)
+                return e.wheelDelta / e.detail / 40 * (e.detail > 0 ? 1 : -1) // Opera
+            else
+                return -e.detail / 3 // Firefox
+        } else
+            return e.wheelDelta / 120 // IE,Safari,Chrome
+    }
+
+    function update() {
+        moving = true
+
+        var delta = (pos - target.scrollTop) / smooth
+
+        target.scrollTop += delta
+
+        if (Math.abs(delta) > 0.5)
+            requestFrame(update)
+        else
+            moving = false
+    }
+
+    var requestFrame = function () { // requestAnimationFrame cross browser
+        return (
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (func) {
+                window.setTimeout(func, 1000 / 50);
+            }
+        );
+    }()
+}
